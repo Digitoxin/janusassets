7fe2b7d8195a2d53fdf551c7f0d2401e5d632a97
Digitalis//Digitoxin <digitalis.digitoxin@gmail.com>
2015-08-19 21:14:23 +0300

    uniforms

--- water3dfrag.glsl
+++ water3dfrag.glsl
@@ -1,3 +1,16 @@
+uniform vec3      iResolution;
+uniform float     iGlobalTime;
+uniform float     iChannelTime[4];
+uniform vec4      iMouse;
+uniform vec4      iDate;
+uniform float     iSampleRate;
+uniform vec3      iChannelResolution[4];
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+
+
 //
 // Description : Array and textureless GLSL 2D/3D/4D simplex 
 //               noise functions.
